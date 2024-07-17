2c552e6f4f7d441493542cb00ed3b75e7b39b5c7
Devendra jangir <120140257+Devendra-jangir@users.noreply.github.com>
2024-07-17 23:07:15 +0530

    Time: 18 ms (61.01%), Space: 44 MB (49.01%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -7,9 +7,9 @@ class Solution {
         int n = nums.length;
         
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicates for j
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
                 int k = j + 1;
                 int l = n - 1;
                 while (k < l) {
@@ -18,8 +18,8 @@ class Solution {
                         ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k - 1]) k++; // skip duplicates for k
-                        while (k < l && nums[l] == nums[l + 1]) l--; // skip duplicates for l
+                        while (k < l && nums[k] == nums[k - 1]) k++; 
+                        while (k < l && nums[l] == nums[l + 1]) l--; 
                     } else if (sum > target) {
                         l--;
                     } else {
