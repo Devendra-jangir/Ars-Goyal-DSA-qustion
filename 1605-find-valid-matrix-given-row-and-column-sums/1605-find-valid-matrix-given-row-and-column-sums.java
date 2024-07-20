f77dd3e987a5f19938bd13f20432068a02d7bec0
Devendra jangir <120140257+Devendra-jangir@users.noreply.github.com>
2024-07-20 15:16:21 +0530

    Time: 1 ms (100.00%), Space: 52.9 MB (53.39%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -5,13 +5,22 @@ class Solution {
         
         int[][] ans = new int[n][m];
         
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                ans[i][j]  = Math.min(rowSum[i], colSum[j]);
-                rowSum[i] -= ans[i][j];
-                colSum[j] -= ans[i][j];
-            }
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         ans[i][j]  = Math.min(rowSum[i], colSum[j]);
+        //         rowSum[i] -= ans[i][j];
+        //         colSum[j] -= ans[i][j];
+        //     }
+        // }
+        
+        for(int i = 0, j = 0; i < n && j < m;) {
+            ans[i][j] = Math.min(rowSum[i], colSum[j]);
+            rowSum[i] -= ans[i][j];
+            colSum[j] -= ans[i][j];
+            if (rowSum[i] == 0) i++;
+            if (colSum[j] == 0) j++;
         }
         return ans;
+
     }
 }
