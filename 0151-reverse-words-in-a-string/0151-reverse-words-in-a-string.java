c396fe184a7a37ec8e85156808c7de847e9ec373
Devendra jangir <120140257+Devendra-jangir@users.noreply.github.com>
2024-07-28 12:52:54 +0530

    Time: 5 ms (92.81%), Space: 45.2 MB (20.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,31 +1,55 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] arr = s.trim().split("\\s+");
+        
+//         int i = 0;
+//         int j = arr.length - 1;
+        
+//         while (i <= j) {
+//             String temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//             i++;
+//             j--;
+//         }
+        
+//         StringBuilder ans = new StringBuilder();
+        
+//         for (int c = 0; c < arr.length; c++) {
+//             ans.append(arr[c]);
+//             if (c < arr.length - 1) {
+//                 ans.append(" ");
+//             }
+//         }
+        
+//         return ans.toString();
+//     }
+// }
+
 class Solution {
     public String reverseWords(String s) {
-        // Trim leading and trailing spaces, then split by one or more spaces
-        String[] arr = s.trim().split("\\s+");
         
-        // Initialize pointers for the start and end of the array
-        int i = 0;
-        int j = arr.length - 1;
-        
-        // Reverse the words in the array
-        while (i <= j) {
-            String temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-        
-        // Use StringBuilder for efficient string concatenation
-        StringBuilder ans = new StringBuilder();
-        
-        for (int c = 0; c < arr.length; c++) {
-            ans.append(arr[c]);
-            if (c < arr.length - 1) {
-                ans.append(" ");
+        int i=s.length()-1;
+        String ans="";
+
+        while(i>=0){
+            //ignore the trailing space of String
+            while(i>=0 && s.charAt(i)==' ') i--;
+            
+            // handle the leading space of String
+            if(i<0) break;
+
+            int j=i;
+            
+            while(i>=0 && s.charAt(i)!=' ') i--;
+
+            if(ans.isEmpty()){
+                ans=ans.concat(s.substring(i+1,j+1));
+            }else{
+                ans=ans.concat(' '+s.substring(i+1,j+1));
             }
         }
-        
-        return ans.toString();
+
+        return ans;
     }
 }
